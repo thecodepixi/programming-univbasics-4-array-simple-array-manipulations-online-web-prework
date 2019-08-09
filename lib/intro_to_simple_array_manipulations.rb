915e1ced3442a12a7bf8a818a791c8ffec2b74e1
Emily Harber <emily.a.harber@gmail.com>
2019-08-09 22:51:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,3 +30,19 @@ def using_insert(array, element)
   array.insert(4, element)
 end 
 
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten 
+end 
+
+def using_delete(array, string)
+  array.delete(string) 
+end 
+
+def using_delete_at(array, int)
+  array.delete(int)
+end 
+
